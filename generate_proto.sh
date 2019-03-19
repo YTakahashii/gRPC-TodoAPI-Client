887a6a6fc0b4488de7f8e2c7164c968f972bab63
Yuta Takahashi <b1015143@fun.ac.jp>
2019-03-19 17:56:27 +0900

    :bug: バグ修正
    # ==== Emojis ====
    # 🎉  :tada: Initial commit
    # 🔖  :bookmark: バージョンタグ
    # 🐛  :bug: バグ修正
    # ✏️  :pencil2: タイポ修正
    # 👍  :+1: 機能改善
    # ✨  :sparkles: 部分的な機能追加
    # 💄  :lipstick: UIやスタイルファイルのアップデート
    # 📝  :memo: ドキュメントやコメントの追加
    # ♻️  :recycle: リファクタリング
    # 🗑️  :wastebasket: 不要な機能・使われなくなった機能の削除
    # ✅  :white_check_mark: テスト実装
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # ➕  :heavy_plus_sign: 依存パッケージの追加
    # ➖  :heavy_minus_sign: 依存パッケージの削除
    # 🆙  :up: 依存パッケージのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🚧  :construction: WIP(Work In Progress)
    # 🙈  :see_no_evil: .gitignoreファイルの更新
    # 🏷️  :label: 型の追加やアップデート
    # 🐳  :whale: Dockerの作業

--- generate_proto.sh
+++ generate_proto.sh
@@ -9,15 +9,8 @@ PROTO_DEST=./src/models/proto
 
 mkdir -p ${PROTO_DEST}
 
-grpc_tools_node_protoc \
-  --js_out=import_style=commonjs,binary:${PROTO_DEST} \
-  --grpc_out=${PROTO_DEST} \
-  --plugin=protoc-gen-grpc=$(which grpc_tools_node_protoc_plugin) \
-  -I ${PROTO_SRC} \
-  ${PROTO_SRC}/*
-
-grpc_tools_node_protoc \
-  --plugin=protoc-gen-ts=$(npm bin)/protoc-gen-ts \
-  --ts_out=${PROTO_DEST} \
+/usr/local/bin/protoc \
+  --js_out=import_style=commonjs:${PROTO_DEST} \
+  --grpc-web_out=import_style=typescript,mode=grpcwebtext:${PROTO_DEST} \
   -I ${PROTO_SRC} \
   ${PROTO_SRC}/*
